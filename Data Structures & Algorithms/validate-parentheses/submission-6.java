1754f9a56a0f3fc2c22627042051dbc0538961b2
SquareLord <105403723+SquareLord@users.noreply.github.com>
2026-07-01 18:04:57 -0400

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        LinkedList<Integer> link = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            switch(s.charAt(i)) {
+                case '(':
+                    link.push(1);
+                    break;
+                case '{':
+                    link.push(2);
+                    break;
+                case '[':
+                    link.push(3);
+                    break;
+                default:
+                    int expec = 0;
+                    if (s.charAt(i) == ')') expec = 1;
+                    if (s.charAt(i) == '}') expec = 2;
+                    if (s.charAt(i) == ']') expec = 3;
+                    if (link.isEmpty()) return false;
+                    Integer n = link.pop();
+                    if (expec != n) return false;
+            }
+        }
+        if (link.isEmpty()) return true;
+        return false;
+    }
+}
